7bc8b3827154f7fba4871ae9dafc7ce7bead54da
김준기 <gugi329@daum.net>
2022-03-18 00:37:56 +0900

    별자리별 로또 번호 생성

--- app/src/main/java/com/icox/lotto/ConstellationActivity.kt
+++ app/src/main/java/com/icox/lotto/ConstellationActivity.kt
@@ -2,8 +2,11 @@ package com.icox.lotto
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.DatePicker
 import androidx.databinding.DataBindingUtil
 import com.icox.lotto.databinding.ActivityConstellationBinding
+import java.util.*
 
 class ConstellationActivity : BaseActivity() {
 
@@ -14,6 +17,56 @@ class ConstellationActivity : BaseActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_constellation)
         setupEvents()
         setValues()
+        val datePicker = binding.datePicker
+        val textView = binding.txtConstellation
+
+        textView.text = makeConstellationString(datePicker.month, datePicker.dayOfMonth)
+        val calendar = Calendar.getInstance()
+        datePicker.init(
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH),
+            object : CalendarView.OnDateChangeListener, DatePicker.OnDateChangedListener {
+                override fun onDateChanged(
+                    view: DatePicker?,
+                    year: Int,
+                    monthOfYear: Int,
+                    dayOfMonth: Int
+                ) {
+                    textView.text = makeConstellationString(datePicker.month, datePicker.dayOfMonth)
+                }
+
+                override fun onSelectedDayChange(
+                    view: CalendarView,
+                    year: Int,
+                    month: Int,
+                    dayOfMonth: Int
+                ) {
+
+                }
+            })
+    }
+
+    fun makeConstellationString(month: Int, day: Int): String {
+//        전달 받은 월/일 정보를 기반으로 정수 형태의 값을 만든다
+//        ex) 1월 5일 -> 105, 11월 1일 -> 1101
+        val target = "${month + 1}${String.format("%02d", day)}".toInt()
+        when (target) {
+            in 101..119 -> return "염소자리"
+            in 120..218 -> return "물병자리"
+            in 219..320 -> return "물고기자리"
+            in 321..419 -> return "양자리"
+            in 420..520 -> return "황소자리"
+            in 521..621 -> return "쌍둥이자리"
+            in 622..722 -> return "게자리"
+            in 723..822 -> return "사자자리"
+            in 823..923 -> return "처녀자리"
+            in 924..1022 -> return "천칭자리"
+            in 1023..1122 -> return "전갈자리"
+            in 1123..1224 -> return "사수자리"
+            in 1225..1231 -> return "염소자리"
+            else -> return "기타 별자리"
+        }
     }
 
     override fun setupEvents() {
