456facc6b6dec57fe2e1abdafa81468711d19010
김준기 <gugi329@daum.net>
2022-03-17 23:57:37 +0900

    HashCode 를 활용한 이름별 로또 번호 생성

--- app/src/main/java/com/icox/lotto/NameActivity.kt
+++ app/src/main/java/com/icox/lotto/NameActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.icox.lotto.databinding.ActivityNameBinding
+import kotlin.random.Random
 
 class NameActivity : BaseActivity() {
 
@@ -16,9 +17,23 @@ class NameActivity : BaseActivity() {
         setValues()
     }
 
+    fun getLottoNumbersFromHash(name: String): MutableList<Int> {
+        val list = mutableListOf<Int>()
+        for (number in 1..45) {
+            list.add(number)
+        }
+        list.shuffle(Random(name.hashCode().toLong()))
+        return list.subList(0, 6)
+    }
+
     override fun setupEvents() {
         binding.btnGo.setOnClickListener {
             val myIntent = Intent(mContext, ResultActivity::class.java)
+            myIntent.putIntegerArrayListExtra(
+                "result",
+                ArrayList(getLottoNumbersFromHash(binding.edtName.text.toString()))
+            )
+            myIntent.putExtra("name", binding.edtName.text.toString())
             startActivity(myIntent)
         }
         binding.btnBack.setOnClickListener {
